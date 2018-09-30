01cb158cd4fc1eae12bb5e8d44771ba887fde13e
Alan Gamboa <alangam97@gmail.com>
2018-09-30 14:53:49 -0500

    no sale :c

--- Program.cs
+++ Program.cs
@@ -4,65 +4,60 @@ namespace Laberinto
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
             int [,] laberinto = new int [5,6];
             int filas;
             int columnas;
             char a, b, d, i;
-            int movimiento;
-             Console.WriteLine("Ingresa tu movimiento:");
-             movimiento = char.Parse(Console.ReadLine());
+            a= 'a';
+            char movimiento;
+            int posicion_inicial;
+            int posicion_actual;
+            int posicion_anterior;
             
-            for(filas =0; filas<5;filas++){
-            for(columnas=0; columnas<6; columnas++){
-                
-                laberinto [filas, columnas] = 0;
-                
-            }
-            
-            
-            laberinto [0,1] =2;
-            
-            /*             laberinto[1,1] =0;
-            laberinto[2,1] =0;
-            laberinto[3,1] =0;
-            laberinto[3,2] =0;
-            laberinto[3,3] =0;
-            laberinto[3,4] = 0;
-            laberinto[4,2] =0;
-            laberinto[2,4] =0;
-            laberinto[1,4] =0;
-            laberinto[0,4] = 0;
-            */
-            
-            for(filas =0; filas<5;filas++){
-                for(columnas =0; columnas<6;columnas++){
-                    
-                    if(movimiento == 'A' || movimiento == 'a'){
-                       
-                        laberinto[filas,columnas] =2;
 
-
-                    }
-                    
-                
+            for(filas =0; filas<5;filas++){
+                for(columnas=0; columnas<6; columnas ++){
+                    laberinto [filas,columnas] =0;
+                    laberinto[0,1] =2;
                     Console.Write("{0}\t",laberinto[filas,columnas]);
                 }
                 Console.WriteLine();
+
             }
             
            
+           Console.WriteLine("Ingresa tu movimiento");
+           movimiento = char.Parse(Console.ReadLine());
+
+          if(movimiento == a){
+              laberinto[0,1]=0;
+              laberinto[1,1] =2;
+              }
+        if(movimiento == a){
+            laberinto[1,1] =0;
+            laberinto[2,1]=2;
+        }
+          for(filas =0; filas<5;filas++){
+                for(columnas=0; columnas<6; columnas ++){
+                    
+                    Console.Write("{0}\t",laberinto[filas,columnas]);
+                }
+                Console.WriteLine();
+}
+            }
                 
 
             
            
         }
     
-    
+    }
 
         
 
-}
-}
-}
+
+
