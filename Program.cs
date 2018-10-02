d8d2365daf46dfc1da832d430b4c85334e229bdc
Alan Gamboa <alangam97@gmail.com>
2018-10-02 08:44:42 -0500

    Ya está shido

--- Program.cs
+++ Program.cs
@@ -8,37 +8,38 @@ namespace Laberinto
         
         static void Main(string[] args)
         {
-            
-            char dezplazamiento;
-            int usuario,x=0,y=1;
-            int[,] matriz =
+                
+            char moverse;
+            int usuario, x = 0, y = 1;
+            int[,] laberinto =
             {
-                {1,0,1,1,0,0 },
+                {1,0,1,1,0,0},
                 {1,0,1,1,0,1},
                 {1,0,1,1,0,1},
                 {1,0,0,0,0,1},
                 {1,1,0,1,1,1}
             };
-            Console.WriteLine("INTRUCCIONES");
-            Console.WriteLine("Tecla 'a' dezplaza un espacio arriba");
-            Console.WriteLine("Tecla 'b' dezplaza un espacio abajo");
-            Console.WriteLine("Tecla 'd' dezplaza un espacio a la derecha");
-            Console.WriteLine("Tecla 'i' dezplaza un espacio a la izquierda");
+            Console.WriteLine("Laberinto");
+            Console.WriteLine("Instrucciones");
+            Console.WriteLine("Tecla 'W' dezplaza un espacio arriba");
+            Console.WriteLine("Tecla 'S' dezplaza un espacio abajo");
+            Console.WriteLine("Tecla 'D' dezplaza un espacio a la derecha");
+            Console.WriteLine("Tecla 'A' dezplaza un espacio a la izquierda");
             Console.WriteLine("El jugador es el numero 2");
-            Console.WriteLine("Llega a la salida");
-            Console.ReadKey();
+            Console.WriteLine("Para ganar llegue al punto (0,5)");
+      
             Console.Clear();
             Console.WriteLine("MAPA");
             for (int i = 0; i < 5; i++)
             {
-                for(int j = 0; j < 6; j++)
+                for (int j = 0; j < 6; j++)
                 {
-                    Console.Write("{0}\t", matriz[i, j]);
+                    Console.Write("{0}\t", laberinto[i, j]);
                 }
                 Console.WriteLine();
             }
-            Console.ReadKey();
-            matriz[x, y] = 2;
+           
+            laberinto[x, y] = 2;
             do
             {
                 Console.Clear();
@@ -46,32 +47,32 @@ namespace Laberinto
                 {
                     for (int j = 0; j < 6; j++)
                     {
-                        Console.Write("{0}\t", matriz[i, j]);
+                        Console.Write("{0}\t", laberinto[i, j]);
                     }
                     Console.WriteLine();
                 }
                 Console.WriteLine("Ingrese direccion de dezplazamiento");
-                dezplazamiento = char.Parse(Console.ReadLine());
-                switch (dezplazamiento)
+                moverse = char.Parse(Console.ReadLine());
+                switch (moverse)
                 {
-                    case 'a':
-                        if (matriz[x-1, y] != 1 && x-1 >= 0)
+                    case 'w':
+                        if (laberinto[x - 1, y] != 1 && x - 1 >= 0)
                         {
-                            matriz[x, y] = 0;
+                            laberinto[x, y] = 0;
                             x = x - 1;
-                            matriz[x, y] = 2;
+                            laberinto[x, y] = 2;
                         }
                         else
                         {
                             Console.WriteLine("No se puede realizar el movimiento");
                         }
                         break;
-                    case 'b':
-                        if (matriz[x + 1, y] != 1 && x+1 <= 5)
+                    case 's':
+                        if (laberinto[x + 1, y] != 1 && x + 1 <= 5)
                         {
-                            matriz[x, y] = 0;
+                            laberinto[x, y] = 0;
                             x = x + 1;
-                            matriz[x, y] = 2;
+                            laberinto[x, y] = 2;
                         }
                         else
                         {
@@ -79,23 +80,23 @@ namespace Laberinto
                         }
                         break;
                     case 'd':
-                        if (matriz[x, y + 1] != 1)
+                        if (laberinto[x, y + 1] != 1)
                         {
-                            matriz[x, y] = 0;
+                            laberinto[x, y] = 0;
                             y = y + 1;
-                            matriz[x, y] = 2;
+                            laberinto[x, y] = 2;
                         }
                         else
                         {
                             Console.WriteLine("No se puede realizar el movimiento");
                         }
                         break;
-                    case 'i':
-                        if (matriz[x, y - 1] != 1)
+                    case 'a':
+                        if (laberinto[x, y - 1] != 1)
                         {
-                            matriz[x, y] = 0;
+                            laberinto[x, y] = 0;
                             y = y - 1;
-                            matriz[x, y] = 2;
+                            laberinto[x, y] = 2;
                         }
                         else
                         {
@@ -103,23 +104,21 @@ namespace Laberinto
                         }
                         break;
                 }
-            } while (matriz[0, 5] != 2);
+            } while (laberinto[0, 5] != 2);
+            Console.WriteLine();
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 6; j++)
                 {
-                    Console.Write("{0}\t", matriz[i, j]);
+                    Console.Write("{0}\t", laberinto[i, j]);
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("!Has Ganado¡");
-                
-
-            
-           
         }
     
     }
+}
 
       
 
